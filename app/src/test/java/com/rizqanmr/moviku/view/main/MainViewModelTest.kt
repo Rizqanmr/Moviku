1bbe17107bf2004e8b6f679aa88b301f8f2577b7
Rizqanmr <45690865+Rizqanmr@users.noreply.github.com>
2024-02-06 02:50:42 +0700

    implement unit test for get genres

---
+++ app/src/test/java/com/rizqanmr/moviku/view/main/MainViewModelTest.kt
@@ -0,0 +1,55 @@
+package com.rizqanmr.moviku.view.main
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.rizqanmr.moviku.network.model.GenresItem
+import com.rizqanmr.moviku.network.model.GenresModel
+import com.rizqanmr.moviku.repository.AppRepository
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class MainViewModelTest {
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val testDispatcher = MainDispatcherRule()
+
+    // Mock dependencies
+    private lateinit var appRepository: AppRepository
+    private lateinit var viewModel: MainViewModel
+
+    @Before
+    fun setUp() {
+        appRepository = mockk()
+        viewModel = MainViewModel(appRepository)
+    }
+
+    @Test
+    fun `getGenres should update live data`() = runBlocking {
+        // Given
+        val fakeGenresModel = fakeGenresModel()
+        coEvery { appRepository.getGenres() } returns fakeGenresModel
+
+        // When
+        viewModel.getGenres()
+
+        // Then
+        assert(viewModel.genres.value == fakeGenresModel)
+    }
+
+    companion object {
+        fun fakeGenresModel() = GenresModel(
+            genres = listOf(
+                GenresItem(name = "action", id = 1),
+                GenresItem(name = "drama", id = 2)
+            )
+        )
+    }
+}
